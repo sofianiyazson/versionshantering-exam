44ae89f23b1d84bde7bfd808d5340a4d5bd788c6
Sofia Niyazson <92554135+sofianiyazson@users.noreply.github.com>
2025-11-05 04:22:14 +0100

    #US2: Show the bookmark list

--- starter/app.js
+++ starter/app.js
@@ -1,16 +1,30 @@
 const form = document.getElementById('bookmarkForm');
+const list = document.getElementById('bookmarkList');
 let bookmarks = [];
 
-form.addEventListener('submit', e => {
-  e.preventDefault(); // Stoppar formuläret från att ladda om sidan
+// Visa alla bokmärken vid start
+    render();
 
+// lägga till ett bokmärke
+form.addEventListener('submit', e => {
+  e.preventDefault();
   const title = document.getElementById('title').value.trim();
   const url = document.getElementById('url').value.trim();
-  if (!title || !url) return; // Avbryt om något fält är tomt
-
-  // Lägg till bokmärket i arrayen
+  if (!title || !url) return;
   bookmarks.push({ title, url });
-
-  form.reset(); // Rensa input-fälten
-  render();     // Uppdatera listan på sidan
+  form.reset();
+  render();
 });
+
+// Visa listan
+    function render() {
+      list.innerHTML = '';
+      bookmarks.forEach((bm, i) => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+          <a href="${bm.url}" target="_blank">${bm.title}</a>
+          <button class="delete" onclick="removeBookmark(${i})">Ta bort</button>
+        `;
+        list.appendChild(li);
+      });
+    }
